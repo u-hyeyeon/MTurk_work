47eb2ef5f216024ec3a97a86478cbd4da40874bf
u.hyeyeon <u.hyeyeon@gmail.com>
2020-04-10 00:31:29 +0900

    [2020.04.10] initial

--- tagging_training_data.py
+++
@@ -1,8 +0,0 @@
-"""
-https://monkeylearn.com/blog/mechanical-turk-101-use-mturk-tagging-training-data/
-    1. How hard and expensive tagging data might be;
-    2. How to get started with MTurk;
-    3. Some best practices for the design of MTurk projects;
-    4. How to publish projects and evaluate their results;
-    5. Some considerations on the interaction with Turkers.
-"""
